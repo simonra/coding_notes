36045aa133a17d77c5dd7178e4c8e43b77dfae03
Simon Randby <simon.randby@gmail.com>
2020-06-24 21:41:29 +0200

    Functional implementation of FizzBuzz in C#

---
+++ cs/optimal-fizbuz/FizzBuzz/FizzBuzz/FizzBuzz.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace FizzBuzz
+{
+    public class FizzBuzz
+    {
+        public string FizzBuzzForNumber(int number)
+        {
+            Func<Func<string, string>, Func<string, string>> fi =
+                f =>
+                {
+                    if(number % 3 == 0)
+                    {
+                        return x => "Fizz" + f("");
+                    }
+                    else
+                    {
+                        return f;
+                    }
+                };
+
+            Func<Func<string, string>, Func<string, string>> bu =
+                f =>
+                {
+                    if (number % 5 == 0)
+                    {
+                        return x => "Buzz" + f("");
+                    }
+                    else
+                    {
+                        return f;
+                    }
+                };
+
+            Func<string, string> self = x => x;
+
+            var result = fi(bu(self))($"{number}");
+            return result;
+
+            //var a = Fizz((a, b) => { return $"{a}{b}"; }, number)("", number);
+            //return a;
+        }
+
+        public Func<string, int, string> Fizz(Func<string, int, string> function, int n)
+        {
+            if(n % 3 == 0)
+            {
+                return (word, number) => { return "Fizz" + function("", n); };
+            }
+            else
+            {
+                return function;
+            }
+        }
+
+        public Func<string, int, string> Buzz(Func<string, int, string> function, int n)
+        {
+            if (n % 5 == 0)
+            {
+                return (word, number) => { return "Buzz" + function("", n); };
+            }
+            else
+            {
+                return function;
+            }
+        }
+    }
+}
---
+++ cs/optimal-fizbuz/FizzBuzz/FizzBuzz/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace FizzBuzz
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var fb = new FizzBuzz();
+            for (int i = 0; i < 36; i++)
+            {
+                Console.WriteLine(fb.FizzBuzzForNumber(i));
+            }
+        }
+    }
+}
